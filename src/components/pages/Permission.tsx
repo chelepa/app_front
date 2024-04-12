64c2f2e2f4ed9889dcf9a8265c185445376c9ee9
Claudimir Chelepa <cl.chelepa@gmail.com>
2024-04-11 23:37:24 -0300

    update 06

--- src/components/pages/Permission.tsx
+++ src/components/pages/Permission.tsx
@@ -5,6 +5,7 @@ import { Message } from '../layout/Message';
 import { useLocation } from 'react-router-dom';
 import { Container } from '../layout/Container';
 import { LinkButton } from '../form/LinkButton';
+import { Table } from 'react-bootstrap';
 
 export const Permission = () => {
     const auth = useContext(PermissionContext);
@@ -19,21 +20,31 @@ export const Permission = () => {
       <div className={styles.container}>
         <div className={styles.title_container}>
           <h1>Modulo de Pemissoes</h1>
-          <LinkButton to='/' text='Nova Permissao'/>
+          <LinkButton to="/" text="Nova Permissao" />
         </div>
-        <Container customClass='start'>
-            <p>teste chelepa ...</p>
+        <Container customClass="start">
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Description</th>
+                <th>Permission</th>
+              </tr>
+            </thead>
+            <tbody>
+              {auth.permissionList.length > 0 &&
+                auth.permissionList.map((project) => (
+                  <tr>
+                    <td>{project.id}</td>
+                    <td>{project.description}</td>
+                    <td>{project.permission}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
         </Container>
-          
       </div>
 
       //    {msg && <Message type={'error'} msg={msg}/>}
-      /* {auth.permissionList.length > 0 && auth.permissionList.map((project) => (
-            <div>
-                <span>{project.id}</span>
-                <span>{project.description}</span>
-                <span>{project.permission}</span>
-            </div>
-           ))} */
     );
 }
